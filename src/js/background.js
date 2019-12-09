fe58f7136e7ac80018838c4094633c0bb9ae074b
Abdulbaki Dursun <eggsydev@outlook.com>
2019-12-09 17:15:30 +0300

    Update 1.0.1, change of lots of stuff!
    This update fixes lots of little bugs running out there.
    The main purpose for this was to make it able to replace the images even after the page loaded! So, if the page you're looking loads images with JavaScript after the page loads, the extension will replace them! It'll try to find new images for 10 times with 1.5 second delay. When it couldn't find any new imgur images in 15 seconds, it'll stop checking!
    And also auto activation of the extension is working now. _It wasn't before, yeah you had to click to popup or visit an imgur image for the first time to get it activated._ But it works now! Wooho!

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,6 @@
+// This will only run when there's no record of extension's active state on localStorage.
+runOnce();
+
 chrome.webRequest.onBeforeRequest.addListener(function (data) {
     const unblocks = localStorage.getItem("imgurUnblocks"),
         latestUnblock = localStorage.getItem("latestUnblock");
@@ -7,7 +10,7 @@ chrome.webRequest.onBeforeRequest.addListener(function (data) {
 
     const activated = localStorage.getItem("extActive") || "true";
 
-    if (activated == "true" && !data.url.includes("https://proxy.duckduckgo.com")) {
+    if (activated == "true" && data.url != "https://imgur.com/" && data.url != "http://imgur.com/" && data.url != "https://imgur.com" && data.url != "http://imgur.com" && !data.url.includes("https://proxy.duckduckgo.com")) {
         localStorage.setItem("imgurUnblocks", (parseInt(unblocks) + 1));
         localStorage.setItem("latestUnblock", Date.now());
 
@@ -22,8 +25,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, reply) {
     else if (request.method == "updateStats" && request.add) {
         addIfExistedOrCreateAndAdd(request.add);
         reply(`Successfully added ${request.add} unblock data! Good job!`);
-    } else reply(false);
+    } else reply("Sorry, unable to replace images. Wait a while and see if they're going to change later, we have 1.5 seconds delay between each changes.");
 });
+
+function runOnce() {
+    if (!localStorage.getItem("extActive")) localStorage.setItem("extActive", true);
+}
 
 function addIfExistedOrCreateAndAdd(value) {
     const exists = localStorage.getItem("imgurUnblocks");
--- src/js/content.js
+++ src/js/content.js
@@ -1,21 +1,38 @@
-chrome.runtime.sendMessage({ method: "getStatus" }, function (response) {
-    if (response.status == "true" && !document.location.href.includes("https://proxy.duckduckgo.com")) {
-        const images = document.querySelectorAll("img");
+let loggedOnce = false,
+    triesLeft = 10;
 
-        if (images) {
-            console.log("Imgur Please\nReplacing all imgur images on the document. Please give us a moment.");
-            let count = 0;
+updateImages();
+const interval = setInterval(updateImages, 1500);
 
-            for (let i in images) {
-                if (images[i] && images[i].attributes && images[i].attributes.src && images[i].attributes.src.textContent.includes("imgur.com/") && !images[i].attributes.src.textContent.includes("proxy.duckduckgo.com")) {
-                    images[i].attributes.src.textContent = "https://proxy.duckduckgo.com/iu/?u=" + images[i].attributes.src.textContent;
-                    count++
+function updateImages() {
+    if (triesLeft >= 0) {
+        chrome.runtime.sendMessage({ method: "getStatus" }, function (response) {
+            if (response.status == "true" && !document.location.href.includes("proxy.duckduckgo.com")) {
+                const images = document.querySelectorAll("img");
+
+                if (images) {
+                    let count = 0;
+
+                    if (!loggedOnce) console.log("Imgur Please\nI'll be looking for imgur images for 15 seconds. Watch the magic!");
+
+                    for (let i in images) {
+                        if (images[i] && images[i].attributes && images[i].attributes.src && images[i].attributes.src.textContent.includes("imgur.com/") && !images[i].attributes.src.textContent.includes("proxy.duckduckgo.com")) {
+                            images[i].attributes.src.textContent = "https://proxy.duckduckgo.com/iu/?u=" + images[i].attributes.src.textContent;
+                            count++
+                        }
+                    }
+
+                    if (count > 0) {
+                        chrome.runtime.sendMessage({ method: "updateStats", add: count }, function (response) {
+                            if (!loggedOnce) console.log(response)
+
+                            // Set logged once to true so it won't spam to your console!
+                            loggedOnce = true;
+                            triesLeft--;
+                        });
+                    } else triesLeft--;
                 }
-            }
-
-            chrome.runtime.sendMessage({ method: "updateStats", add: count }, function (response) {
-                console.log(response)
-            });
-        }
-    }
-});
+            } else clearInterval(interval);
+        });
+    } else clearInterval(interval);
+}
